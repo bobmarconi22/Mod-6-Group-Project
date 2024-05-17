d5a751b9fe5231afee83a48848e4468fa9a547c8
Bob <bobmarconi22@gmail.com>
2024-05-17 18:02:55 -0400

    suer model/seeder updated

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,12 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .shops import seed_shops, undo_shops
+from .selected_categories import seed_selected_categories, undo_selected_categories
+from .reviews import seed_reviews, undo_reviews
+from .menus import seed_menus, undo_menus
+from .images import seed_images, undo_images
+from .categories import seed_categories, undo_categories
+from .addresses import seed_addresses, undo_addresses
 
 from app.models.db import db, environment, SCHEMA
 
@@ -12,12 +19,26 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
+        undo_shops()
+        undo_selected_categories()
+        undo_reviews()
+        undo_menus()
+        undo_images()
+        undo_categories()
+        undo_addresses()
     seed_users()
+    seed_shops()
+    seed_selected_categories()
+    seed_reviews()
+    seed_menus()
+    seed_images()
+    seed_categories()
+    seed_addresses()
     # Add other seed functions here
 
 
@@ -25,4 +46,11 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_shops()
+    undo_selected_categories()
+    undo_reviews()
+    undo_menus()
+    undo_images()
+    undo_categories()
+    undo_addresses()
     # Add other undo functions here
