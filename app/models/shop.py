88b2cc850eebb659cdb2aa02bb12d1b86a0b979f
Bob <bobmarconi22@gmail.com>
2024-05-20 13:16:26 -0400

    render?

--- app/models/shop.py
+++ app/models/shop.py
@@ -13,7 +13,7 @@ class Shop(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     name = db.Column(db.String(40), nullable=False)
     description = db.Column(db.String(255), nullable=False)
     hours = db.Column(JSON, nullable=False)
