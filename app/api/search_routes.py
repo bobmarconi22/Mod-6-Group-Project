c82898f2aa788323ad64fb3995128469aee24c1c
Shelbey Casalena <shelbey.casalena@gmail.com>
2024-05-21 15:31:28 -0700

    nav bar changes in the middle but trying to fix my pull mess thing

---
+++ app/api/search_routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify
+from app.models import db, Shop, selected_categories, Category
+from sqlalchemy.orm import joinedload
+from .utils import find_avg
+
+
+search_routes = Blueprint('search', __name__)
+
+
+@search_routes.route('/', methods=['POST'])
+def search_shops():
+    # query will always be shops- if there's a name we ignore the other parameters
+    pass
