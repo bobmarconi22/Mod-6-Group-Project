0479b419fda48f0ec1bd1fc788b871e3f1dc87d2
Andrew Ly <andrew.ly205@gmail.com>
2024-05-21 01:18:58 -0700

    Created shop detail page

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -4,6 +4,7 @@ import SignupFormPage from '../components/SignupFormPage';
 import LandingPage from '../components/LandingPage'
 import Layout from './Layout';
 import CreateShop from '../components/CreateShop';
+import ShopDetail from '../components/ShopDetail';
 
 export const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ export const router = createBrowserRouter([
       {
         path: "/create-shop",
         element: <CreateShop />
+      },
+      {
+        path: 'shops/:id',
+        element: <ShopDetail />
       }
     ],
   },
