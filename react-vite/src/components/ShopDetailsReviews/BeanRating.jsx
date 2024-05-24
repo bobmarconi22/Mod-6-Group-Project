f44f2ff8ff61ee0b8512fc46ae8146207ac38b8b
Shelbey Casalena <shelbey.casalena@gmail.com>
2024-05-23 22:13:09 -0700

    removing imports

--- react-vite/src/components/ShopDetailsReviews/BeanRating.jsx
+++ react-vite/src/components/ShopDetailsReviews/BeanRating.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { CiCoffeeBean } from "react-icons/ci";
 import { BiSolidCoffeeBean } from "react-icons/bi";
 // import '../ReviewModals/BeanRating.css'
@@ -6,12 +6,12 @@ import { BiSolidCoffeeBean } from "react-icons/bi";
 
 const BeanRating = ({ rating }) => {
 
-  const [activeRating, setActiveRating] = useState(0)
+  // const [activeRating, setActiveRating] = useState(0)
 
   return (
     <div className='five-beans'>
       {[1, 2, 3, 4, 5].map(i => (
-        <div>
+        <div key={i}>
           {rating >= i ?
             < BiSolidCoffeeBean className='custom-icon' />
             : < CiCoffeeBean className='custom-icon' />}
