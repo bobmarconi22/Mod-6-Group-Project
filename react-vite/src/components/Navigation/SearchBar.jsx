d534757d2ef780a6868e268741780ed5512a8761
Shelbey Casalena <shelbey.casalena@gmail.com>
2024-06-11 13:37:56 -0700

    search bar dynamic sizing

--- react-vite/src/components/Navigation/SearchBar.jsx
+++ react-vite/src/components/Navigation/SearchBar.jsx
@@ -3,17 +3,22 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-
 function SearchBar() {
-    const [category, setCategory] = useState("");
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [name, setName] = useState('')
-    const [priceRange, setPriceRange] = useState({"1": false, "2": false, "3": false, "4": false, "5": false})
-    const allCategories = useSelector((state) => state.categories.categories);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const [category, setCategory] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [name, setName] = useState("");
+  const [priceRange, setPriceRange] = useState({
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+  });
+  const allCategories = useSelector((state) => state.categories.categories);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    useEffect( () => {
+  useEffect(() => {
     const fetchCategories = async () => {
       await dispatch(getAllCategories());
       setIsLoaded(true);
@@ -21,85 +26,127 @@ function SearchBar() {
     fetchCategories();
   }, [dispatch]);
 
-   const handlePriceRangeToggle = (e) => {
+  const handlePriceRangeToggle = (e) => {
     const value = e.target.value;
     setPriceRange((prevPriceRange) => ({
       ...prevPriceRange,
-      [value]: !prevPriceRange[value]
+      [value]: !prevPriceRange[value],
     }));
   };
 
   const clear = (e) => {
-    e.preventDefault()
-    setCategory("")
-    setName("")
-    setPriceRange({"1": false, "2": false, "3": false, "4": false, "5": false})
-    navigate('/')
-  }
+    e.preventDefault();
+    setCategory("");
+    setName("");
+    setPriceRange({ 1: false, 2: false, 3: false, 4: false, 5: false });
+    navigate("/");
+  };
 
-// Search
-  const handleSearch= async (e) => {
-    e.preventDefault()
-    let priceRangeArr = Object.keys(priceRange).filter(num => priceRange[num] === true)
+  // Search
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    let priceRangeArr = Object.keys(priceRange).filter(
+      (num) => priceRange[num] === true
+    );
 
-    const query = {}
-    if (priceRangeArr.length > 0) query.priceRange = priceRangeArr.join(',')
-    if(name.length > 0) query.name = name
-    if(category.length > 0) query.category = category
+    const query = {};
+    if (priceRangeArr.length > 0) query.priceRange = priceRangeArr.join(",");
+    if (name.length > 0) query.name = name;
+    if (category.length > 0) query.category = category;
 
-    const params = new URLSearchParams(query)
+    const params = new URLSearchParams(query);
 
+    navigate(`/search?${params.toString()}`);
+  };
 
-    navigate(`/search?${params.toString()}`)
-  }
-
-
-    return (
+  return (
     isLoaded && (
       <div className="search-bar-div">
         <form onSubmit={(e) => handleSearch(e)} className="search-bar">
-            <div>
-          <input placeholder="Shop Name" value={name} onChange={(e)=> setName(e.target.value)}></input>
+          {/* <div className="top-half-search"> */}
+          <div className="name-search">
+            <input
+              placeholder="Shop Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
           </div>
-          <div >
-          <label className="category-label">
-            <select
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-            >
-                <option value="" >All Categories</option>
-              {Object.entries(allCategories).map((category, index) => (
-                <option
-                  key={index}
-                  value={category[1].name}
-                >
-                  {category[1].name}
-                </option>
-              ))}
-            </select>
-          </label>
-
+          <div className="catagory-search">
+            <label className="category-label">
+              <select className="catagory-select"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="">All Categories</option>
+                {Object.entries(allCategories).map((category, index) => (
+                  <option key={index} value={category[1].name}>
+                    {category[1].name}
+                  </option>
+                ))}
+              </select>
+            </label>
           </div>
-          <div className="price-range-div">
-            <button type="button" onClick={(e) => handlePriceRangeToggle(e)} value="1" id="left-button" className={priceRange["1"] ? "dollar-filled left-button" : "dollar left-button"}>
-              &#36;
+          {/* </div> */}
+          {/* <div className="bottom-half-search"> */}
+          <div className="price-range-search">
+            <div className="price-range-div">
+              <button
+                type="button"
+                onClick={(e) => handlePriceRangeToggle(e)}
+                value="1"
+                id="left-button"
+                className={
+                  priceRange["1"]
+                    ? "dollar-filled left-button"
+                    : "dollar left-button"
+                }
+              >
+                &#36;
+              </button>
+              <button
+                type="button"
+                onClick={(e) => handlePriceRangeToggle(e)}
+                value="2"
+                className={priceRange["2"] ? "dollar-filled" : "dollar"}
+              >
+                &#36; &#36;
+              </button>
+              <button
+                type="button"
+                onClick={(e) => handlePriceRangeToggle(e)}
+                value="3"
+                className={priceRange["3"] ? "dollar-filled" : "dollar"}
+              >
+                &#36; &#36; &#36;
+              </button>
+              <button
+                type="button"
+                onClick={(e) => handlePriceRangeToggle(e)}
+                value="4"
+                id="right-button"
+                className={
+                  priceRange["4"]
+                    ? "dollar-filled right-button"
+                    : "dollar right-button"
+                }
+              >
+                &#36; &#36; &#36; &#36;
+              </button>
+            </div>
+          </div>
+          <div className="submit-buttons">
+            <button id="search-submit" type="submit">
+              Search
             </button>
-             <button type="button" onClick={(e) => handlePriceRangeToggle(e)} value="2" className={priceRange["2"] ? "dollar-filled" : "dollar"}>
-              &#36; &#36;
-            </button>
-             <button type="button" onClick={(e) => handlePriceRangeToggle(e)} value="3" className={priceRange["3"] ? "dollar-filled" : "dollar"}>
-              &#36; &#36; &#36;
-            </button>
-             <button type="button" onClick={(e) => handlePriceRangeToggle(e)} value="4" id="right-button" className={priceRange["4"] ? "dollar-filled right-button" : "dollar right-button"}>
-              &#36; &#36; &#36; &#36;
+            <button id="search-submit" type="submit" onClick={(e) => clear(e)}>
+              Clear
             </button>
           </div>
-          <button id="search-submit" type="submit">Search</button>
-          <button id="search-submit" type="submit" onClick={(e) => clear(e)}>Clear</button>
+          {/* </div> */}
         </form>
       </div>
     )
   );
 }
 
-export default SearchBar
+export default SearchBar;
